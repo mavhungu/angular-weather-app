b23ecdea14183fc654a6d88d4dc89ec378a0e849
ronewa <ronewamavhungu@gmail.com>
2024-04-29 13:03:18 +0200

    removing stylesheet selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit{
   title = 'weather-app';
